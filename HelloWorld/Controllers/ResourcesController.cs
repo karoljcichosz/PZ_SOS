bec24b114a0194a854ed50e654c7628255c19039
Michal <bumi050496@gmail.com>
2018-11-14 17:27:25 +0100

    first architectural draft

---
+++ HelloWorld/Controllers/ResourcesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        //
+        // GET: /Resources/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
