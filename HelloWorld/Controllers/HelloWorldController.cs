d266ead6dce8e23b03d23f09a484b6f6f53ff2a9
Michal <bumi050496@gmail.com>
2018-11-14 15:49:03 +0100

    added hello world

---
+++ HelloWorld/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        [HttpGet]
+        public ViewResult HelloWorld()
+        {
+            return View();
+        }
+    }
+}
